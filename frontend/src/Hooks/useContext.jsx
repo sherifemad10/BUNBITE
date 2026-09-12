d609c091f9cd197a6ae3aa7e4a93007e58bbf662
sherifemad10 <sherifemad.web@gmail.com>
2026-09-12 22:58:42 +0300

    editForDeploy

--- frontend/src/Hooks/useContext.jsx
+++ frontend/src/Hooks/useContext.jsx
@@ -5,7 +5,7 @@ import { DataContext } from "./DataContext";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 // check token
